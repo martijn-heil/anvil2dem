d5f97e606d624c9a19c99322a6409801fa73e37d
Martijn Heil <m.heil375@gmail.com>
2019-06-04 15:15:50 +0200

    Improve description of project

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@
   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
   SOFTWARE.
 
-    nin-anvil - Generate a heightmap from a .mca file.
+  nin-anvil - Generate a DEM from .mca files.
 */
 
 #include <stdlib.h>
